5ec8e72b6e43374ec2e4b693b30ece0472860813
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-07 23:04:50 +0530

    beginning

---
+++ PastPaper-BICT/src/main/java/com/example/PastPaperBICT/PastPaperBictApplication.java
@@ -0,0 +1,13 @@
+package com.example.PastPaperBICT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastPaperBictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastPaperBictApplication.class, args);
+	}
+
+}
---
+++ PastPaper-BICT/src/test/java/com/example/PastPaperBICT/PastPaperBictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PastPaperBICT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastPaperBictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
