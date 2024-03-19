e7b70cdbcb424c434d1f76def95c3fdd0bfa2c05
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-19 23:05:53 +0530

    update

--- PastPaper-BICT/src/main/java/Entity/Paper.java
+++ PastPaper-BICT/src/main/java/Entity/Paper.java
@@ -22,5 +22,51 @@ public class Paper {
     @Column(nullable = false)
     private String coursecode;
 
+    public String getPaper_id() {
+        return paper_id;
+    }
 
+    public void setPaper_id(String paper_id) {
+        this.paper_id = paper_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCoursecode() {
+        return coursecode;
+    }
+
+    public void setCoursecode(String coursecode) {
+        this.coursecode = coursecode;
+    }
+
+    public Paper(String paper_id, String title, String subject, String year, String coursecode) {
+        this.paper_id = paper_id;
+        this.title = title;
+        this.subject = subject;
+        this.year = year;
+        this.coursecode = coursecode;
+    }
 }
