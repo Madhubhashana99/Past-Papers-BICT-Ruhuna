37971c7c81a9a2ee2aa65424e9dfc0a658db46eb
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-19 22:14:48 +0530

    update

--- PastPaper-BICT/src/main/java/Entity/Download.java
+++ PastPaper-BICT/src/main/java/Entity/Download.java
@@ -1,4 +1,58 @@
 package Entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Download {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @ManyToOne
+    @JoinColumn(name="paper_id", nullable = false)
+    private Paper paper;
+
+    private Long downloadCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Entity.Paper getPaper() {
+        return paper;
+    }
+
+    public void setPaper(Entity.Paper paper) {
+        this.paper = paper;
+    }
+
+    public Long getDownloadCount() {
+        return downloadCount;
+    }
+
+    public void setDownloadCount(Long downloadCount) {
+        this.downloadCount = downloadCount;
+    }
+
+    public Download(Long id, String fileName, Entity.Paper paper, Long downloadCount) {
+        this.fileName = fileName;
+        this.paper = paper;
+    }
 }
