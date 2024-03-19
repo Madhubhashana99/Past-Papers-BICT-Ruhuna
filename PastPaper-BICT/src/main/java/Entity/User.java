b10e4aa26e83d415869cc24a94d52a3c25efb6db
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-19 23:10:22 +0530

    update

--- PastPaper-BICT/src/main/java/Entity/User.java
+++ PastPaper-BICT/src/main/java/Entity/User.java
@@ -1,4 +1,28 @@
 package Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @Column(length = 5,nullable = false)
+    private String user_id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String role;
+
+    @Column(nullable = false, length = 100) // Consider appropriate length based on password complexity
+    private String password;
+
+
+
 }
