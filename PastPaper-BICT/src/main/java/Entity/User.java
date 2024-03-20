e7e00c61569dd830212e7a50e10d41464a12d086
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-20 22:23:20 +0530

    update

--- PastPaper-BICT/src/main/java/Entity/User.java
+++ PastPaper-BICT/src/main/java/Entity/User.java
@@ -23,6 +23,51 @@ public class User {
     @Column(nullable = false, length = 100) // Consider appropriate length based on password complexity
     private String password;
 
+    public String getUser_id() {
+        return user_id;
+    }
 
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String user_id, String username, String email, String role, String password) {
+        this.user_id = user_id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
 }
